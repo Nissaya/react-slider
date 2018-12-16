9cec2a2153f67b31cba3946abfcd32962171111f
Vladislav Lukashov <lukashov9182@gmail.com>
2018-12-16 15:03:40 +0300

    'react'

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <FullName name='Vlad' surname='Lukashov' link='vk.com' />
+        <FullName name='Ann' surname='Ivanova' link='google.com' />
+        <FullName name='Ivan' surname='Petrov' link='facebook.com' />
       </div>
     );
   }
+}
+
+function FullName(props) {
+  return(
+    <div>
+      <h1>My name is {props.name}, my surname is {props.surname}</h1>
+      <a href={props.link}>Lik for my profile</a>
+    </div>
+  )
 }
 
 export default App;
